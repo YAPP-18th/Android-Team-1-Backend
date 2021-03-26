26b5af16f4ec1855f9944ea777a91991218131e0
NaLDo627 <naruhodo627@gmail.com>
2021-03-26 15:32:20 +0900

    feat: initial commit

---
+++ src/main/java/net/murreng/murreng/MurrengBackendApplication.java
@@ -0,0 +1,13 @@
+package net.murreng.murreng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MurrengBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MurrengBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/murreng/murreng/MurrengBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package net.murreng.murreng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MurrengBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
