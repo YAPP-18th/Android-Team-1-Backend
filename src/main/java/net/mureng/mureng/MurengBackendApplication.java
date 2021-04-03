92ab09753f20766b7f50ddf51f973c5498624a90
catnap421 <wlq7410@naver.com>
2021-04-03 15:17:28 +0900

    feat: rename project

--- src/main/java/net/mureng/mureng/MurengBackendApplication.java
+++ src/main/java/net/mureng/mureng/MurengBackendApplication.java
@@ -1,13 +1,13 @@
-package net.murreng.murreng;
+package net.mureng.mureng;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MurrengBackendApplication {
+public class MurengBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MurrengBackendApplication.class, args);
+		SpringApplication.run(MurengBackendApplication.class, args);
 	}
 
 }
