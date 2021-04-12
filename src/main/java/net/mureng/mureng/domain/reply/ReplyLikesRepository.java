7d47d5ee5419f0e1dc425a3cbcecff38daa35d01
catnap421 <wlq7410@naver.com>
2021-04-12 23:00:00 +0900

    Add: 답변 좋아요 테스트 작성

--- src/main/java/net/mureng/mureng/domain/reply/ReplyLikesRepository.java
+++ src/main/java/net/mureng/mureng/domain/reply/ReplyLikesRepository.java
@@ -3,4 +3,6 @@ package net.mureng.mureng.domain.reply;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReplyLikesRepository extends JpaRepository<ReplyLikes, ReplyLikesPK> {
+    Long countAllByIdReplyId(Long replyId);
+    Long countByIdReplyId(Long replyId);
 }
