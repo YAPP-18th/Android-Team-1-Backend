afd488e1c04698a9ac5ad7404807e1eadf041566
catnap421 <wlq7410@naver.com>
2021-04-12 15:33:59 +0900

    Add: 단어 힌트 저장소 추가 및 테스트 추가

--- src/main/java/net/mureng/mureng/domain/question/WordHintRepository.java
+++ src/main/java/net/mureng/mureng/domain/question/WordHintRepository.java
@@ -2,5 +2,9 @@ package net.mureng.mureng.domain.question;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface WordHintRepository extends JpaRepository<WordHint, Long> {
+    List<WordHint> findAllByQuestionId(Question questionId);
+
 }
