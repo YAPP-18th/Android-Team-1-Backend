17be388cf6c904d4181173870d6a1ab749a88663
catnap421 <wlq7410@naver.com>
2021-04-12 23:06:38 +0900

    Remove: ReplyLikesRepository 'countAll' 메서드 삭제

--- src/main/java/net/mureng/mureng/domain/reply/ReplyLikesRepository.java
+++ src/main/java/net/mureng/mureng/domain/reply/ReplyLikesRepository.java
@@ -3,6 +3,5 @@ package net.mureng.mureng.domain.reply;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReplyLikesRepository extends JpaRepository<ReplyLikes, ReplyLikesPK> {
-    Long countAllByIdReplyId(Long replyId);
     Long countByIdReplyId(Long replyId);
 }
