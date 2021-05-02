faab9b5b1af4bd537631d88831938aa928b04b4f
NaLDo627 <naruhodo627@gmail.com>
2021-05-02 22:00:27 +0900

    fix: swagger-ui 시큐리티 우회

--- src/main/java/net/mureng/mureng/core/config/SecurityConfig.java
+++ src/main/java/net/mureng/mureng/core/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import net.mureng.mureng.core.jwt.JwtTokenProvider;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -35,5 +36,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         .anyRequest().authenticated()
                 .and()
                     .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/v2/api-docs", "/configuration/ui",
+                                "/swagger-resources", "/configuration/security",
+                                "/swagger-ui.html", "/webjars/**","/swagger/**"); 
     }
 }
