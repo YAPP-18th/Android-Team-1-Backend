b05526a4bc55a0f90dd83873edd6469e511c8b92
catnap421 <wlq7410@naver.com>
2021-05-18 16:47:04 +0900

    Add: ReplyMapperTest 추가

--- src/test/java/net/mureng/mureng/common/EntityCreator.java
+++ src/test/java/net/mureng/mureng/common/EntityCreator.java
@@ -1,8 +1,12 @@
 package net.mureng.mureng.common;
 
+import net.mureng.mureng.member.dto.MemberDto;
 import net.mureng.mureng.member.entity.Member;
+import net.mureng.mureng.question.dto.QuestionDto;
+import net.mureng.mureng.question.dto.WordHintDto;
 import net.mureng.mureng.question.entity.Question;
 import net.mureng.mureng.question.entity.WordHint;
+import net.mureng.mureng.reply.dto.ReplyDto;
 import net.mureng.mureng.reply.entity.Reply;
 import net.mureng.mureng.reply.entity.ReplyLikes;
 
@@ -10,6 +14,7 @@ import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class EntityCreator {
     private static final Long QUESTION_ID = 1L;
@@ -65,6 +70,60 @@ public class EntityCreator {
                 .member(createMemberEntity())
 //                .reply(createReplyEntity())
                 .regDate(LocalDateTime.of(2020, 10, 14, 17, 11, 9))
+                .build();
+    }
+
+    public static MemberDto.ReadOnly createMemberDto() {
+        return MemberDto.ReadOnly.builder()
+                .memberId(1L)
+                .identifier("123")
+                .email("test@gmail.com")
+                .nickname("Test")
+                .murengCount(0)
+                .attendanceCount(10)
+                .lastAttendanceDate("2020-10-14")
+                .isPushActive(true)
+                .build();
+    }
+
+    public static QuestionDto.ReadOnly createQuesetionDto() {
+        return QuestionDto.ReadOnly.builder()
+                .questionId(1L)
+                .category("카테고리")
+                .content("This is english content.")
+                .koContent("이것은 한글 내용입니다.")
+                .wordHints(Set.of(createWordHintDto()))
+                .repliesCount(2)
+                .build();
+    }
+
+    public static ReplyDto.ReadOnly createReplyDto() {
+        return ReplyDto.ReadOnly.builder()
+                .questionId(QUESTION_ID)
+                .replyId(REPLY_ID)
+                .author(createMemberDto())
+                .question(createQuesetionDto())
+                .content("Test Reply")
+                .image("image-path")
+                .replyLikeCount(2)
+                .build();
+    }
+
+    public static WordHint createWordHint() {
+        return  WordHint.builder()
+                .hintId(1L)
+                .question(Question.builder().build())
+                .word("apple")
+                .meaning("사과")
+                .regDate(LocalDateTime.parse("2020-10-14T11:00:00"))
+                .build();
+    }
+
+    public static WordHintDto.ReadOnly createWordHintDto() {
+        return WordHintDto.ReadOnly.builder()
+                .hintId(1L)
+                .word("apple")
+                .meaning("사과")
                 .build();
     }
 }
--- src/test/java/net/mureng/mureng/reply/component/ReplyMapperTest.java
+++ src/test/java/net/mureng/mureng/reply/component/ReplyMapperTest.java
@@ -1,7 +1,39 @@
 package net.mureng.mureng.reply.component;
 
-import static org.junit.jupiter.api.Assertions.*;
+import net.mureng.mureng.common.EntityCreator;
+import net.mureng.mureng.reply.dto.ReplyDto;
+import net.mureng.mureng.reply.entity.Reply;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
 class ReplyMapperTest {
+
+    @Autowired
+    ReplyMapper replyMapper;
+
+    private final Reply reply = EntityCreator.createReplyEntity();
+    private final ReplyDto.ReadOnly replyDto = EntityCreator.createReplyDto();
+
+    @Test
+    public void 엔티티에서_DTO변환_테스트() {
+        ReplyDto.ReadOnly mappedDto = replyMapper.toDto(reply);
+        assertEquals(replyDto.getReplyId(), mappedDto.getReplyId());
+        assertEquals(replyDto.getContent(), mappedDto.getContent());
+        assertEquals(replyDto.getContent(), mappedDto.getContent());
+        assertEquals(replyDto.getAuthor().getMemberId(), mappedDto.getAuthor().getMemberId());
+        assertEquals(replyDto.getQuestion().getQuestionId(), mappedDto.getQuestion().getQuestionId());
+        assertEquals(replyDto.getReplyLikeCount(), mappedDto.getReplyLikeCount());
+    }
+
+    @Test
+    public void DTO에서_엔티티변환_테스트() {
+        Reply mappedEntity = replyMapper.toEntity(replyDto);
+        assertEquals(reply.getContent(), mappedEntity.getContent());
+        assertEquals(reply.getImage(), mappedEntity.getImage());
+    }
 
 }
