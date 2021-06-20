9d0c690a6f2be5876f908b6b1d5b965bafd7d25c
NaLDo627 <naldo627@github.com>
2021-06-21 00:56:21 +0900

    Fix: 토큰 중복 이슈 수정

--- mureng-core/src/main/java/net/mureng/core/member/repository/FcmTokenRepository.java
+++ mureng-core/src/main/java/net/mureng/core/member/repository/FcmTokenRepository.java
@@ -6,5 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface FcmTokenRepository extends JpaRepository<FcmToken, Long> {
+    boolean existsByToken(String token);
     Optional<FcmToken> findByMemberMemberId(Long memberId);
+    Optional<FcmToken> findByToken(String token);
 }
--- mureng-core/src/main/java/net/mureng/core/member/service/FcmTokenService.java
+++ mureng-core/src/main/java/net/mureng/core/member/service/FcmTokenService.java
@@ -16,11 +16,22 @@ public class FcmTokenService {
 
     @Transactional
     public void insertToken(String token) {
+        if (fcmTokenRepository.existsByToken(token)) {
+            return;
+        }
         fcmTokenRepository.saveAndFlush(FcmToken.builder().token(token).build());
     }
 
     @Transactional
     public void updateTokenOfMember(String token, Member member) {
+        // 토큰이 이미 존재한다면, 요청자 토큰으로 업데이트 한다.
+        if (fcmTokenRepository.existsByToken(token)) {
+            FcmToken fcmToken = fcmTokenRepository.findByToken(token).orElseThrow();
+            fcmToken.setMember(member);
+            fcmTokenRepository.saveAndFlush(fcmToken);
+            return;
+        }
+
         // 토큰이 존재하지 않으면 새로 생성
         Optional<FcmToken> fcmTokenOptional = fcmTokenRepository.findByMemberMemberId(member.getMemberId());
         if (fcmTokenOptional.isEmpty()) {
