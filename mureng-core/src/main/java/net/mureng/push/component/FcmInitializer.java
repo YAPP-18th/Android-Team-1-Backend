9f79b0171b436b78901a06c10b8ba9fd43b649f4
NaLDo627 <naldo627@github.com>
2021-06-22 10:16:34 +0900

    Fix: FCM 키가 Base64 인코딩 된 경우 디코딩 프로세스 추가

--- mureng-core/src/main/java/net/mureng/push/component/FcmInitializer.java
+++ mureng-core/src/main/java/net/mureng/push/component/FcmInitializer.java
@@ -11,7 +11,10 @@ import org.springframework.util.StringUtils;
 import javax.annotation.PostConstruct;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.util.Base64;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Slf4j
 @Component
@@ -23,6 +26,10 @@ public class FcmInitializer {
     public void initialize()  {
         if (! StringUtils.hasText(firebaseServiceKey)) {
             return;
+        }
+
+        if (isBase64Encoded(firebaseServiceKey)) {
+            firebaseServiceKey = new String(Base64.getDecoder().decode(firebaseServiceKey));
         }
 
         try {
@@ -37,5 +44,11 @@ public class FcmInitializer {
         } catch (IOException e) {
             log.error(e.getMessage());
         }
+    }
+
+    private boolean isBase64Encoded(String key) {
+        Pattern pattern = Pattern.compile("^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)?$");
+        Matcher matcher = pattern.matcher(key);
+        return matcher.find();
     }
 }
