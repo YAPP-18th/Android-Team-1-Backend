b7a0eb66be1eccb7f0f7bb87f4398d6f0a5b1352
catnap421 <wlq7410@naver.com>
2021-06-24 18:03:46 +0900

    Add: 뱃지 확인 엔드포인트 추가

--- mureng-api/src/main/java/net/mureng/api/member/service/MemberBadgeService.java
+++ mureng-api/src/main/java/net/mureng/api/member/service/MemberBadgeService.java
@@ -2,19 +2,32 @@ package net.mureng.api.member.service;
 
 import lombok.RequiredArgsConstructor;
 import net.mureng.core.badge.entity.BadgeAccomplished;
+import net.mureng.core.badge.entity.BadgeAccomplishedPK;
 import net.mureng.core.badge.repository.BadgeAccomplishedRepository;
+import net.mureng.core.core.exception.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-@Service
 @RequiredArgsConstructor
+@Service
 public class MemberBadgeService {
     private final BadgeAccomplishedRepository badgeAccomplishedRepository;
 
     @Transactional(readOnly = true)
     public List<BadgeAccomplished> getMemberBadges(Long memberId){
         return badgeAccomplishedRepository.findBadgeAccomplishedsByIdMemberId(memberId);
+    }
+
+    @Transactional
+    public boolean updateBadgeAccomplished(Long memberId, Long badgeId){
+        BadgeAccomplishedPK pk = BadgeAccomplishedPK.builder().memberId(memberId).badgeId(badgeId).build();
+        BadgeAccomplished badgeAccomplished = badgeAccomplishedRepository.findById(pk).orElseThrow(() -> new ResourceNotFoundException("존재하지 않는 리소스에 대한 요청입니다."));
+        badgeAccomplished.setIsChecked(true);
+
+        badgeAccomplishedRepository.saveAndFlush(badgeAccomplished);
+
+        return badgeAccomplished.getIsChecked();
     }
 }
