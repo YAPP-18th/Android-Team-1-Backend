dc7e91957d91cf5bed624a28d46789b9ed06e627
catnap421 <wlq7410@naver.com>
2021-07-09 16:38:37 +0900

    Fix: 쿠키 중복 획득 에러 수정

--- mureng-core/src/main/java/net/mureng/core/cookie/repository/CookieAcquirementRepository.java
+++ mureng-core/src/main/java/net/mureng/core/cookie/repository/CookieAcquirementRepository.java
@@ -3,6 +3,9 @@ package net.mureng.core.cookie.repository;
 import net.mureng.core.cookie.entity.CookieAcquirement;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public interface CookieAcquirementRepository extends JpaRepository<CookieAcquirement, Long> {
-    Long countByMemberMemberId(Long memberId);
+    boolean existsByRegDateAndMemberMemberId(LocalDate date, Long memberId);
 }
--- mureng-core/src/main/java/net/mureng/core/reply/service/ReplyPostProcessServiceImpl.java
+++ mureng-core/src/main/java/net/mureng/core/reply/service/ReplyPostProcessServiceImpl.java
@@ -1,23 +1,38 @@
 package net.mureng.core.reply.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import net.mureng.core.cookie.repository.CookieAcquirementRepository;
 import net.mureng.core.cookie.service.CookieAcquirementService;
+import net.mureng.core.core.exception.BadRequestException;
 import net.mureng.core.member.entity.Member;
 import net.mureng.core.member.repository.MemberRepository;
 import net.mureng.core.reply.entity.Reply;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+import static net.mureng.core.core.message.ErrorMessage.ALREADY_ANSWERED_MEMBER;
+import static net.mureng.core.core.message.ErrorMessage.ALREADY_COOKIE_ACQUIRED;
+
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ReplyPostProcessServiceImpl implements ReplyPostProcessService {
     private final MemberRepository memberRepository;
-    private final CookieAcquirementService cookieAchievementService;
+    private final CookieAcquirementService cookieAcquirementService;
+    private final CookieAcquirementRepository cookieAcquirementRepository;
 
     @Transactional
     public void postProcess(Reply reply) {
         Member member = reply.getAuthor();
-        cookieAchievementService.acquireMurengCookie(member);
+        if( !cookieAcquirementService.isAlreadyCookieAcquiredToday(member.getMemberId()) ) {
+            log.info(ALREADY_COOKIE_ACQUIRED);
+            cookieAcquirementService.acquireMurengCookie(member);
+        }
 
         memberRepository.saveAndFlush(member);
     }
